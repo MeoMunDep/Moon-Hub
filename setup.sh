7525ce70236cfb06650835b9c8e21198090c8264
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-27 02:18:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,13 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "upgradeMining": 5,
+  "upgradeStorage": 5,
+  "delayEachAccount": [1, 81],
+  "referralCode": "ABuexiAm89DVdi"
 }
 EOL
 }
@@ -55,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Moonhub BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +112,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd "moon hub" && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
